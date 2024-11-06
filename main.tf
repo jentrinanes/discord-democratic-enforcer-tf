5774cfaf6e963c0f83cf7b2bc62c55deaeadfb0b
Jen Triñanes <jen.trinanes@whitecobalt.com>
2024-11-07 00:11:36 +0800

    Create Resource Group

---
+++ main.tf
@@ -0,0 +1,21 @@
+# Configure the Azure provider
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 4.7.0"
+    }
+  }
+
+  required_version = ">= 1.1.0"
+}
+
+provider "azurerm" {
+  features {}
+  subscription_id = var.subscription_id
+}
+
+resource "azurerm_resource_group" "rg" {
+  name     = var.resource_group_name
+  location = var.location
+}
---
+++ variables.tf
@@ -0,0 +1,11 @@
+variable "resource_group_name" {
+  default = "rg-discord-democratic-enforcer"
+}
+
+variable "location" {
+  default = "southeastasia"
+}
+
+variable "subscription_id" {
+  default = ""
+}
